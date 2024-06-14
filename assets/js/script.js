1c40fc079df1350039f2ec01a8b1b3f4c9ae689a
Katalin Molnar <molnarlin@gmail.com>
2024-06-14 14:11:33 +0100

    memory game updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,74 @@
-const gridContainer = document.querySelector(".grid-container");
-let game_cards=[];
+const cards = document.querySelectorAll('.game-cards')
 
-let firstCard, secondCard;
+let hasFlippedCard = false;
 let lockBoard = false;
-
-fetch("cards.js")
-    .then((data) => {
-        game_cards = [...data, ...data];
-        shuffleCards();
-        generateCards();
-    });
-
-function shuffleCards(){
-    let currentIndex = game_cards.length,
-    randomIndex,
-    temporaryValue;
- while(currentIndex !== 0){
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = game_cards[currentIndex];
-    game_cards[currentIndex] = game_cards[randomIndex];
-    game_cards[randomIndex] = temporaryValue;
- }
-}
-
-function generateCards(){
-    for(let game_card of game_cards){
-        const cardElement = document.createElement("div");
-        cardElement.classList.add("game_card");
-        cardElement.setAttribute("data-name". game_card.name);
-        cardElement.innerHTML = `
-        <div class="front">
-        <img class="front-image src=${game_card.image} />
-        </div>
-        <div class="back"></div>`;
-        gridContainer.appendChild(cardElement);
-        cardElement.addEventListener("click", flipCard);
-    }
-}
+let firstCard, secondCard;
 
 function flipCard(){
     if(lockBoard) return;
     if(this === firstCard) return;
+    
+    this.classList.toggle('flip');
 
-    this.classList.add("flipped");
+    if(!hasFlippedCard)
+    {
+        //First Card Click
+        hasFlippedCard = true
+        firstCard = this
 
-    if(!firstCard) {
-        firstCard = this;
         return;
     }
 
-    secondCard = this;
-    lockBoard = true;
+    //Second Card Click
+    secondCard = this
 
-    checkForMatch();
+    checkIfMatch()
 }
 
-function checkForMatch(){
-    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+function checkIfMatch(){
 
-    isMatch ? disableCards() : unflipCards();
+        let isMatch = firstCard.dataset.match === secondCard.dataset.match;
+        
+        isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards(){
-    firstCard.removeEventListener("click", flipCard);
-    secondCard.removeEventListener("click", flipCard);
+    firstCard.removeEventListener('click', flipCard)
+    secondCard.removeEventListener('click', flipCard)
 
-    resetBoard();
+    resetBoard()
 }
 
 function unflipCards(){
-    setTimeout(() => {
-        firstCard.classList.remove("flipped");
-        secondCard.classList.remove("flipped");
-        resetBoard();
-    }, 1000);
+    lockBoard = true;
+    
+    setTimeout( () => {
+        firstCard.classList.remove('flip')
+        secondCard.classList.remove('flip')
+        lockBoard = false;
+        resetBoard()
+    }, 1500)
 }
+
+function resetBoard(){
+    hasFlippedCard = false;
+    lockBoard = false;
+    firstCard = null;
+    secondCard = null;
+}
+
+/**This function executes immediately after its definition
+ * because it is wrapped in a parenthesis and 
+ * has an extra pair of parenthesis at its end.
+ */
+(function shuffle(){
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 20)
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard))
 
 function resetBoard(){
     firstCard = null;
@@ -90,6 +83,22 @@ function restart(){
     generateCards();
 }
 
+function waitForClick(){
+    return new Promise(resolve => {
+        const element = document.getElementById("startButton");
+        const handler = () => {
+            element.removeEventListener("click", handler);
+            resolve();
+        };
+        element.addEventListener("click", handler);
+    });
+}
+
+async function startGame(){
+    await waitForClick();
+}
+
+startGame();
 /*progress bar from W3 School*/
 
 var i = 0;
